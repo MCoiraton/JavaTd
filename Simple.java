2d51b58c7f3cbbe8f718897075c166d532c3cef2
Clement Hinz <clement.hinz@epitech.eu>
2024-04-11 10:59:12 +0200

    Rajout class simple

---
+++ Simple.java
@@ -0,0 +1,40 @@
+package TD1;
+
+class Simple extends Compte{
+    private double TAUX_INTERET = 5;
+
+    public Simple(double solde, TypeCarte typeCarte) {
+        super(solde, typeCarte);
+    }
+    
+    protected void effectuerTransaction(double montant, double taxe) {
+        solde -= (montant + taxe);
+        totalTaxe += taxe;
+        nombreTransactions++;
+    }
+    public void versement(double montantTTC){
+        montantTTC= montantTTC-5;
+        if (solde - montantTTC >= 0) {
+           super(montantTTC);
+        } else {
+            System.out.println("Opération impossible : solde insuffisant.");
+        }
+    }
+    public void retrait(double montantTTC){
+       montantTTC= montantTTC-5;
+        if (solde - montantTTC >= 0) {
+           super(montantTTC);
+        } else {
+            System.out.println("Opération impossible : solde insuffisant.");
+        }
+    }
+    public void virement(double montantTTC, Compte receveur){
+       montantTTC= montantTTC-5;
+        if (solde - montantTTC >= 0) {
+           super(montantTTC);
+        } else {
+            System.out.println("Opération impossible : solde insuffisant.");
+        }
+    }
+    
+}
