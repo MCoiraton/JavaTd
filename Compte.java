c590db9b36882dc237c866e6daa50b6f39e36402
MCoiraton <mathis.coiraton@gmail.com>
2024-04-11 11:06:18 +0200

    ajout cpte payant

--- Compte.java
+++ Compte.java
@@ -5,12 +5,19 @@ abstract class Compte  {
     private int code;
     private double solde;
     private TypeCarte typeCarte;
+    private static double montantBanque;
+    private static int nbOperations;
 
     public Compte(double solde, TypeCarte typeCarte) {
         nbCompte++;
         this.code=nbCompte;
         this.solde = solde - typeCarte.getCost();
+        setMontantBanque(typeCarte.getCost());
         this.typeCarte = typeCarte;
+    }
+
+    public static void setMontantBanque(double montantBanque) {
+        Compte.montantBanque = montantBanque;
     }
 
     public double getSolde() {
@@ -22,19 +29,25 @@ abstract class Compte  {
 
     public void versement(double montantTTC){
         this.solde+=montantTTC;
+        nbOperations++;
     }
 
     public void retrait(double montantTTC){
         this.solde-=montantTTC;
+        nbOperations++;
     }
     public void virement(double montantTTC, Compte receveur){
         retrait(montantTTC);
         receveur.versement(montantTTC);
+        nbOperations++;
     }
 
-    public String CompteToString() {
+    public String compteToString() {
         return "Compte numéro"+code+" [solde=" + solde + ", typeCarte=" + typeCarte + "]";
     }
     
+    public String banqueToString(){
+        return "montant ammassé par la banque : "+montantBanque+" pour " +nbOperations+" operations";
+    }
     
 }
--- Payant.java
+++ Payant.java
@@ -1,11 +1,31 @@
 package TD1;
 
 public class Payant extends Compte{
+    private double decouvert;
 
-    public Payant(double solde, TypeCarte typeCarte) {
-        
-        super(solde, typeCarte);
-        //TODO Auto-generated constructor stub
+    public Payant(double solde, TypeCarte typeCarte, double decouvert) {
+        super(solde-50, typeCarte);
+        setMontantBanque(50);
+        this.decouvert=decouvert;
     }
 
+    public void versement(double montant){
+        setMontantBanque(1);
+        super.versement(montant-1);
+    }
+
+    public void retrait(double montant){
+        montant+=1;
+        setMontantBanque(1);
+        if (super.getSolde()-montant>=decouvert)
+            super.retrait(montant);
+        else System.out.println("découvert atteint");    
+    }
+    public void virement(double montant, Compte receveur){
+        montant+=1;
+        setMontantBanque(1);
+        if (super.getSolde()-montant>=decouvert)
+            super.virement(montant, receveur);
+        else System.out.println("découvert atteint");   
+    }
 }
